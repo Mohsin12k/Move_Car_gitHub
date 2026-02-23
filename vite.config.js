d6547472666e7e9ff88dcd8115f13c2d17263751
Mohsin <mk773404@gmail.com>
2026-02-23 12:40:15 +0500

    vite_config_js is updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/movingcar_gitHub_uploaded_gitHub_gh_pages/'
+   base: '/movingcar_gitHub_uploaded/'
 })
